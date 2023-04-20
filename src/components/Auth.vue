2807806bc54ab970152a0fabc5228aae9c32294e
Luthfi Kurniawan <luthkur@gmail.com>
2023-04-21 05:12:27 +0700

    feat: change to comifuro 16

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -1,7 +1,7 @@
 <template>
   <form class="row flex flex-center" @submit.prevent="handleLogin">
     <div class="col-6 form-widget">
-      <h1 class="header">Comivuro 2 Circle Catalog Dashboard</h1>
+      <h1 class="header">Comifuro 16 Circle Catalog Dashboard</h1>
       <p class="description">Sign in with your email and the password provided in the email sent to your inbox</p>
       <div>
         <input
